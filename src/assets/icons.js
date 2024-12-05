5544343d1d40d89c56144fcfbb54ceb5e05ae980
unknown <yc0519165>
2024-12-05 19:04:13 +0530

    update

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,7 +1,7 @@
-import menu_icon from "/src/assets/menu_icon.png";
-import home_icon from "/src/assets/home_icon.png";
-import logo from "/src/assets/logo.png";
-import user from "/src/assets/Image 4.png";
+import menu_icon from "src/assets/menu_icon.png";
+import home_icon from "src/assets/home_icon.png";
+import logo from "src/assets/logo.png";
+import user from "src/assets/Image 4.png";
 
 export const icons = {
   menu_icon,
